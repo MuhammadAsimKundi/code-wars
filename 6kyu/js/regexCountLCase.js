279ef6da13c86ad8be76d5e5f93ce02286935328
MuhammadAsimKundi <muhammadasimkundi@gmail.com>
2025-05-25 23:58:01 +0500

    count number of lowercase using regix

---
+++ 6kyu/js/regexCountLCase.js
@@ -0,0 +1,23 @@
+// Your task is simply to count the total number of lowercase letters in a string.
+
+// Examples
+// "abc" ===> 3
+
+// "abcABC123" ===> 3
+
+// "abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 3
+
+// "" ===> 0;
+
+// "ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 0
+
+// "abcdefghijklmnopqrstuvwxyz" ===> 26
+
+// code
+
+function lowercaseCount(str){
+    //How many?
+  let letters= str.match(/[a-z]/g)
+  return letters ? letters.length : 0;
+  
+}
